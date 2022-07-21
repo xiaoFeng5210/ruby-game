5f4dd65f638288da8e413ba4ee7205f41cf4acfd
Eadweard <804875728@qq.com>
2022-07-21 23:02:09 +0800

    api 设计完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post '/users/', to: 'users#create'
-  get '/users/:id', to: 'users#show'
+  # post '/users/', to: 'users#create'
+  # get '/users/:id', to: 'users#show'
 
-  post '/validation_codes', to: 'validationCodes#create'
+  # post '/validation_codes', to: 'validationCodes#create'
   namespace :api do
     namespace :v1 do
-      resources :validation_codes
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+      
     end
   end
 end
