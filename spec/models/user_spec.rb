deae86147020e47fb7f1d5944fa4456fe7637444
Eadweard <804875728@qq.com>
2022-07-24 22:17:31 +0800

    to eq

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it '有 email' do
     user = User.new email: 'xiaofeng@1.com'
-    expect(user.email).to be 'xiaofeng@1.com'
+    expect(user.email).to eq 'xiaofeng@1.com'
   end
 end
