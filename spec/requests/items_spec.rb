a54dd549912173abe4a16e21e2e19bc513da7005
Eadweard <804875728@qq.com>
2022-07-24 22:33:01 +0800

    查询接口测试

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,10 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Items", type: :request do
   describe "GET /items" do
-    it "works! (now write some real specs)" do
-      get '/api/v1/items'
+    it "index by page" do
+      11.times do
+        Item.create amount: 100
+      end
+      expect(Item.count).to eq(11)
+      get "/api/v1/items"
       expect(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq(10)
     end
   end
 end
