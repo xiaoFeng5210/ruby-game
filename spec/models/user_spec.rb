15646d4b4b4cfd4d962442bff5f4e296a83b3768
Eadweard <804875728@qq.com>
2022-08-27 19:17:49 +0800

    创建一个验证码存入数据库中

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it '有 email' do
-    user = User.new email: 'xiaofeng@1.com'
-    expect(user.email).to eq 'xiaofeng@1.com'
+    user = User.new email: 'm15105215616@163.com'
+    expect(user.email).to eq 'm15105215616@163.com'
   end
 end
